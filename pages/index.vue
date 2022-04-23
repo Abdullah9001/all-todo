17907c3b7fbc088211f70a4ddc0431c08dd0797d
Abdullah9001 <abdullahjubayer44@gmail.com>
2022-04-23 22:40:27 +0600

    chenge something in home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="container">
-    <h1>This is Home Page</h1>
+    <h1>Hello everyone, Thanks for coming hare..</h1>
+    <hr />
+    <h3>Click any of this picture and go to this page.</h3>
+    <NuxtLink to="/bookpage"><img src="book.JPG" /></NuxtLink>
+    <NuxtLink to="/todo"><img src="todo.JPG" /></NuxtLink>
+    <NuxtLink to="/dogpage"><img src="dog.JPG" /></NuxtLink>
   </div>
 </template>
 
@@ -11,8 +16,17 @@ export default {
 </script>
 
 <style scoped>
-  .container {
-    max-width: 900px;
-    margin: 0 auto;
-  }
+.container {
+  max-width: 900px;
+  margin: 0 auto;
+}
+
+img {
+  width: 250px;
+  height: 250px;
+  object-fit: cover;
+  margin: 10px;
+  padding: 5px;
+  border: 2px dotted #ccc;
+}
 </style>
